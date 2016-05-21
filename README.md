2cef405807b9f15979465fb76170fd1880e529f3
Patrik Jansson <patrik.ja@gmail.com>
2016-05-21 21:46:20 +0200

    Some notes and a bit of specification code for computation of synthetic populations

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # SyntheticPopulations
 Some material and code for generating simple synthetic populations.
+
+Links:
+* simPop:  https://cran.r-project.org/web/packages/simPop/index.html
+* PoPGen:  http://urbanmodel.asu.edu/popgen.html
+* SynthPop:https://github.com/UDST/synthpop
+
+
+Related: software to do the simulation of synthetic population:
+* Fred:    http://fred.publichealth.pitt.edu/about.php
---
+++ src/Draft.hs
@@ -0,0 +1,93 @@
+module Draft where
+import Prelude hiding (length)
+import qualified Data.List as L (genericLength)
+
+length = L.genericLength
+
+{-
+
+Background: we assume a population fullPop of fullN real agents. Each
+agent is a record of type A with a few attributes (labels). Each
+attribute can be classified into a few discrete ordered bins. A (1-D)
+histogram is mapping from bins to (natural) numbers and from fullPop
+we can easily compute a histogram for each attribute. It is also
+straightforward to define 2-D (or n-D) histograms for pairs (or
+n-tuples) of attributes. Part of the input for computing the synthetic
+population is a collection of (mostly 1-D) histograms for a few
+attributes of the full population. (But usually fullPop is not
+available - only some histograms of fullPop.)
+
+We also assume access to a subset ps of the population. This has
+(normally all) data from someN real agents but usually someN << fullN.
+
+The synthetic population is constructed to have
+1. the same histograms as the full population and
+2. similar correlations as the subset ps
+
+Usually the synthetic population (with n attributes) is constructed in
+three steps:
+
+1. first an n-D histogram smallHist is contructed from the subset ps.
+2. Then smallHist is scaled up to bigHist to match the collection of
+   histograms for fullPop.
+3. Finally a collection of individual agents are constructed to have
+   bigHist as their n-D histogram.
+
+Example: Assume we have only two attributes, Age and Income.
+
+-}
+
+
+data Age    = Child | Mid | Old      deriving (Eq, Ord, Show, Bounded, Enum)
+data Income = Poor | Middle | Rich   deriving (Eq, Ord, Show, Bounded, Enum)
+
+type Hist1 a    = a -> Nat
+type Hist2 a b  = a -> b -> Nat
+
+histogram1 :: Eq a => (p -> a) -> Many p -> Hist1 a
+histogram1 proj ps p = length (filter ((p==).proj) ps)
+
+histogram2 :: (Eq a, Eq b) => (p -> a) -> (p -> b) -> Many p -> Hist2 a b
+histogram2 proja projb ps a b = length (filter (\p -> a == proja p && b == projb p) ps)
+
+hist2hista :: All b => Hist2 a b -> Hist1 a
+hist2hista h2 = \a -> sumAll (\b -> h2 a b)
+
+hist2histb :: All a => Hist2 a b -> Hist1 b
+hist2histb h2 = \b -> sumAll (\a -> h2 a b)
+
+-- Specification of the desired answer
+checkSyntPopHist :: (All a, All b) => Hist1 a -> Hist1 b -> Hist2 a b -> Hist2 a b -> Bool
+checkSyntPopHist ha hb smallHist bigHist =
+     close epsilon ha (hist2hista bigHist) &&
+     close epsilon hb (hist2histb bigHist) &&
+     closeToProportional eps smallHist bigHist
+
+-- ----------------------------------------------------------------
+
+type Many = []
+type Nat = Double -- sometimes Int works, but when scaling up we will
+                  -- get to real numbers.
+
+eps = 1e-4
+epsilon = 1e-9
+
+sumAll :: (Bounded a, Enum a) => Hist1 a -> Nat
+sumAll   ha = sum (map    ha  [minBound .. maxBound])
+
+sumMap :: (Bounded a, Enum a) => (Double -> Double) -> Hist1 a -> Double
+sumMap f ha = sum (map (f.ha) [minBound .. maxBound])
+
+class (Bounded a, Enum a) => All a
+
+closeToProportional :: (All a, All b) => Double -> Hist2 a b -> Hist2 a b -> Bool
+closeToProportional diff smallHist bigHist = undefined
+  -- TODO: check that there is some scalar k such that  bigHist ~= k .* smallHist
+
+close :: All a => Double -> Hist1 a -> Hist1 a -> Bool
+close eps h1 h2 = sumMap (^2) (minus h1 h2) <= eps^2
+
+minus :: Hist1 a -> Hist1 a -> Hist1 a
+minus h1 h2 a = h1 a - h2 a
+
+-- ----------------------------------------------------------------
